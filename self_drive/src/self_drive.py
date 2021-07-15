a23a26a9c9db691e2202386402ff9b3c676b7d3e
JKLee <dlwhdrlf0619@naver.com>
2021-07-15 16:45:11 +0900

    edit sensor range_2

--- self_drive/src/self_drive.py
+++ self_drive/src/self_drive.py
@@ -13,6 +13,7 @@ class SelfDrive:
     def lds_callback(self, scan):
         turtle_vel = Twist()
 
+        point_forward = scan.ranges[0]
         forward = (average(scan.ranges[0:18])+average(scan.ranges[342:359]))/2
         semi_left = average(scan.ranges[19:54])
         left = average(scan.ranges[55:90])
@@ -21,22 +22,20 @@ class SelfDrive:
 
         print("forward", forward, "\nsemi right", semi_right, "\nright", right, "\nsemi left", semi_left, "\nleft", left, "\n")
 
-        if forward > 0.35:
-            turtle_vel.linear.x = 0.15
+        if forward > 0.6 or point_forward > 0.3:
+            turtle_vel.linear.x = 0.2
             turtle_vel.angular.z = 0
-        elif forward < 0.35:
+            if right < 0.25:
+                turtle_vel.angular.z = -1.6
+            if left < 0.25:
+                turtle_vel.angular.z = 1.6
+        elif forward < 0.6 or point_forward < 0.3 or semi_right < 0.4 or semi_left < 0.4:
             if semi_right > semi_left:
-                turtle_vel.linear.x = 0.05
-                turtle_vel.angular.z = -0.5
-                if right < 0.12:
-                    turtle_vel.angular.z = -1
+                turtle_vel.angular.z = -1.4
             elif semi_left > semi_right:
-                turtle_vel.linear.x = 0.05
-                turtle_vel.angular.z = 0.5
-                if left < 0.12:
-                    turtle_vel.angular.z = 1
+                turtle_vel.angular.z = 1.4
 
-        #self.publisher.publish(turtle_vel)
+        self.publisher.publish(turtle_vel)
 
 
 def average(some_list):
@@ -45,7 +44,7 @@ def average(some_list):
 
 def main():
     rospy.init_node('self_drive')
-    publisher = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+    publisher = rospy.Publisher('cmd_vel', Twist, queue_size=5)
     driver = SelfDrive(publisher)
     subscriber = rospy.Subscriber('scan', LaserScan,
                                   lambda scan: driver.lds_callback(scan))
