a02b964a427b86410bc92bad08b58e67643fba16
JKLee <dlwhdrlf0619@naver.com>
2021-07-15 14:04:00 +0900

    edit sensor range

--- self_drive/src/self_drive.py
+++ self_drive/src/self_drive.py
@@ -17,24 +17,26 @@ class SelfDrive:
         turtle_vel = Twist()
 
         forward = (average(scan.ranges[0:18])+average(scan.ranges[342:359]))/2
-        semi_right = average(scan.ranges[19:54])
-        right = average(scan.ranges[55:90])
-        semi_left = average(scan.ranges[307:342])
-        left = average(scan.ranges[270:306])
+        semi_left = average(scan.ranges[19:54])
+        left = average(scan.ranges[55:90])
+        semi_right = average(scan.ranges[307:342])
+        right = average(scan.ranges[270:306])
 
-        if forward > 0.25:
+        print("forward", forward, "\nsemi right", semi_right, "\nright", right, "\nsemi left", semi_left, "\nleft", left, "\n")
+
+        if forward > 0.35:
             turtle_vel.linear.x = 0.15
             turtle_vel.angular.z = 0
-        elif forward < 0.25:
+        elif forward < 0.35:
             if semi_right > semi_left:
                 turtle_vel.linear.x = 0.15
                 turtle_vel.angular.z = -0.1
-                if right < 0.1:
+                if right < 0.12:
                     turtle_vel.angular.z = -0.3
             elif semi_left > semi_right:
                 turtle_vel.linear.x = 0.15
                 turtle_vel.angular.z = 0.1
-                if left < 0.1:
+                if left < 0.12:
                     turtle_vel.angular.z = 0.3
 
         #self.publisher.publish(turtle_vel)
